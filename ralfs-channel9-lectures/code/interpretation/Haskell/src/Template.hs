c365bfc627107d0776ae0d7ec6d592e3b8ba6c64
rlaemmel <rlaemmel@80f8bc9e-4d75-439a-9954-e8dd026db917>
2010-08-24 16:48:02 +0000

    First commit for the "Evolution of interpreter" code base.

---
+++ ralfs-channel9-lectures/code/interpretation/Haskell/src/Template.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+#include "Imports.hs"
+#include "Syntax.hs"
+#include "Domains.hs"
+#include "Value.hs"
+#include "Interpreter.hs"
+#include "Algebra.hs"
+#include "Library.hs"
+#include "Locals.hs"
+#include "Test.hs"
+#include "Main.hs"
