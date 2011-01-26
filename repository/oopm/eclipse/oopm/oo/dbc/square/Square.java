b01a6811ccef58a4f24f269aebb000f021905dbf
rlaemmel <rlaemmel@80f8bc9e-4d75-439a-9954-e8dd026db917>
2011-01-26 16:28:47 +0000

    Added a DBC example

---
+++ repository/oopm/eclipse/oopm/oo/dbc/square/Square.java
@@ -0,0 +1,31 @@
+package oo.dbc.square;
+
+public class Square extends Rectangle {
+	
+	protected boolean invariant() {
+		return super.invariant() && getWidth() == getHeight();
+	}
+
+	public Square(int length) {
+		super(length, length);
+	}
+	
+	public int getLength() {
+		return getWidth(); 
+	}
+
+	public void setLength(int length) {
+		setWidth(length); 
+	}
+	
+	public void setHeight(int length) { 		
+		setWidth(length);
+	}		
+	
+	public void setWidth(int length) { 		
+		assert length >= 0; // Precondition
+		this.width = length; 
+		this.height = length; 
+		assert invariant();
+	}
+}
