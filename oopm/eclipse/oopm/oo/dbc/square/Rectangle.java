4d48f1e155a5c86eb3296a85ead102da4c2473c8
rlaemmel <rlaemmel@80f8bc9e-4d75-439a-9954-e8dd026db917>
2011-01-26 16:28:47 +0000

    Added a DBC example

---
+++ oopm/eclipse/oopm/oo/dbc/square/Rectangle.java
@@ -0,0 +1,42 @@
+package oo.dbc.square;
+
+public class Rectangle {
+
+	protected int width;
+	protected int height;
+	
+	protected boolean invariant() {
+		return getWidth() >= 0 && getHeight() >= 00;
+	}
+
+	public Rectangle(int width, int heigth) {
+		assert width >= 0 && height >= 0; // Precondition
+		this.width = width;
+		this.height = heigth;
+		assert invariant();
+	}
+	
+	public int getWidth() {
+		return width; 
+	}
+	
+	public int getHeight() { 
+		return height; 
+	}
+
+	public void setWidth(int width) { 		
+		assert width >= 0; // Precondition
+		this.width = width; 
+		assert invariant();
+	}
+	
+	public void setHeight(int height) { 
+		assert height >= 0; // Precondition
+		this.height = height; 
+		assert invariant();
+	}
+
+	public int getPerimeter() {
+		return (width+height)*2;
+	}
+}
