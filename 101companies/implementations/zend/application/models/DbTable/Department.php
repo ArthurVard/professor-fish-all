1c5607b2722963d96a8fdee39dff05b9a11a4e01
tzimmer <tzimmer@80f8bc9e-4d75-439a-9954-e8dd026db917>
2011-11-28 21:51:50 +0000

    zend

--- 101companies/implementations/zend/application/models/DbTable/Department.php
+++ 101companies/implementations/zend/application/models/DbTable/Department.php
@@ -8,21 +8,29 @@ class Application_Model_DbTable_Department extends Zend_Db_Table_Abstract
     public function getDepartmentsForCompany($id) {
         $id = (int)$id;
         $rows = $this->fetchAll('cid = ' . $id . ' AND did IS NULL');
-        
-        $result = array();
-        foreach ($rows as $row) {
-            $result[] = $row->name;
-        }
         return $rows->toArray();
     }
     
-        public function getDepartment($id) {
+    public function getDepartmentsForDepartment($id) {
+        $id = (int)$id;
+        $rows = $this->fetchAll('did = ' . $id);
+        return $rows->toArray();
+    }
+    
+    public function getDepartment($id) {
         $id = (int)$id;
         $row = $this->fetchRow('id = ' . $id);
         if (!$row) {
             throw new Exception("Could not find row $id");
         }
         return $row->toArray();
+    }
+    
+    public function updateDepartment($id, $name) {
+        $data = array(
+            'name' => $name
+        );
+        $this->update($data, 'id = '. (int) $id);
     }
 }
 
